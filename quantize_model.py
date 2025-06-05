1ebb541f3dd1fe81bb9b7ca0cb9dce6ff3403b85
Carolinesvanborg <144431813+Carolinesvanborg@users.noreply.github.com>
2025-06-05 15:38:44 +0200

    Update yaml quantize_model.py

--- quantize_model.py
+++ quantize_model.py
@@ -1,12 +1,23 @@
 import torch
+import yaml
 from torch.quantization import quantize_dynamic
 from train import SentimentModel  # importer din modelklasse
 import nltk
 from nltk.tokenize import word_tokenize
 nltk.download('punkt')
 
-# Indlæs trænet model
-model = SentimentModel()
+# Load konfiguration
+with open("config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+
+# Initialiser model med konfigurationsparametre
+model = SentimentModel(
+    vocab_size=config["vocab_size"],
+    embedding_dim=config["embedding_dim"],
+    hidden_dim=config["hidden_dim"],
+    output_dim=config["output_dim"],
+    max_len=config["max_len"]
+)
 model.load_state_dict(torch.load("best_model.pth"))
 model.eval()
 

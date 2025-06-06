b9261c0929beee52c9941878265207574b7d22ee
boletteeh <144413549+boletteeh@users.noreply.github.com>
2025-06-06 21:15:28 +0200

    Update lr5 targeted_unlearning.py

--- targeted_unlearning.py
+++ targeted_unlearning.py
@@ -38,7 +38,7 @@ dataloader = DataLoader(subset, batch_size=64, shuffle=True)
 
 # === 4. Gradient ascent ===
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=1e-3)
+optimizer = optim.SGD(model.parameters(), lr=1e-5)
 
 epochs = 5
 for epoch in range(epochs):

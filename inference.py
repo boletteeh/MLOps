d3d95891c6ec165acf2226d6b79bc04d3ac19ff6
Carolinesvanborg <144431813+Carolinesvanborg@users.noreply.github.com>
2025-06-06 09:15:47 +0200

    Update get tokens right inference.py

--- inference.py
+++ inference.py
@@ -18,7 +18,9 @@ def run_single_inference():
     tokens = preprocess_text(sample_text)
 
     # Byg ord-til-indeks mapping baseret på testdata
-    word2idx = build_word2idx_from_tokens([tokens])
+     with open("word2idx.pkl", "rb") as f:
+        word2idx = pickle.load(f)
+    
     indices = [word2idx.get(token, word2idx['<UNK>']) for token in tokens]
     padded = pad_sequence(indices, max_len=69)
     input_tensor = torch.tensor([padded], dtype=torch.long)
